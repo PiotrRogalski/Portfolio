33bb74560a9f5e3d958b2ea2f3f3f6b0bee2ef63
Piotr Rogalski <piotr5rogalski@gmail.com>
2018-12-21 10:01:28 +0100

    Add Technologies: Model, Controller and Migration

---
+++ app/Http/Controllers/TechnologyController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Technology $technology)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Technology $technology)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Technology  $technology
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Technology $technology)
+    {
+        //
+    }
+}
