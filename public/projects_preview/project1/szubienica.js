be71db0ace653e63387f8ad4c6a42f9a70105439
Piotr Rogalski <piotr5rogalski@gmail.com>
2019-09-26 19:42:53 +0200

    Update existing project number 1

--- public/projects_preview/project1/szubienica.js
+++ public/projects_preview/project1/szubienica.js
@@ -1,101 +1,212 @@
-var haslo = "Co masz zrobić jutro zrób dzisiaj";
-haslo = haslo.toUpperCase();
+/*
+Based on Mirosław Zelent code.
+Modified by author at 2019-09-26
+Author Piotr Rogalski <piotr5rogalski@gmail.com>
+ */
 
-var dlugosc = haslo.length;
-var ile_skuch = 0;
+const PROVERBS = [
+	'apetyt rośnie w miarę jedzenia',
+	'baba z wozu koniom lżej',
+	'baju baju będziesz w raju',
+	'będzie z niego ksiądz jak z diabła kościelny',
+	'bęben dlatego tak głośny bo próżny',
+	'bez pracy nie ma kołaczy',
+	'biednemu zawsze wiatr w oczy',
+	'broda mędrcem nie czyni',
+	'cel uświęca środki',
+	'chlebem i solą ludzie ludzi niewolą',
+	'ciągnie swój do swego',
+	'cicha woda brzegi rwie',
+	'ciekawość to pierwszy stopień do piekła',
+	'co kraj to obyczaj',
+	'co nagle to po diable',
+	'co za dużo to niezdrowo',
+	'człowiek strzela Pan Bóg kule nosi',
+	'czym skorupka za młodu nasiąknie tym na starość trąci',
+	'darowanemu koniowi w zęby się nie zagląda',
+	'diabeł nie śpi',
+	'diabeł tkwi w szczegółach',
+	'dla chcącego nic trudnego',
+	'do trzech razy sztuka',
+	'dopóty dzban wodę nosi dopóki mu się ucho nie urwie',
+	'dzieci i ryby głosu nie mają',
+	'elektryka prąd nie tyka',
+	'fortuna kołem się toczy'
+];
 
-var haslo1 = "";
+resetProverbs();
+function getProverb()
+{
+	if(proverbs.length < 1)
+	{
+		resetProverbs();
+		numWins = 0;
+	}
 
-for (i = 0;i < dlugosc; i++){
-	if (haslo.charAt(i) == " ")haslo1 = haslo1 + " ";
-	else haslo1 = haslo1 + "-";
+	let key = Math.floor(Math.random() * proverbs.length);
+	let proverb = proverbs[key];
+	proverbs.splice(key, 1);
+
+	return proverb.toUpperCase();
 }
 
+
+function hidePassword(password)
+{
+	let hidden = '';
+	for (i = 0; i < password.length; ++i)
+	{
+		let letter = ' ';
+		if (password.charAt(i) !== ' ')
+		{
+			letter = "_";
+		}
+		hidden = hidden + letter;
+	}
+	return hidden
+}
+
+const FAIL_LIMIT = 9;
+var numWins = 0;
+var haslo = getProverb();
+var numFails = 0;
+var hiddenPassword = hidePassword(haslo);
 var yes = new Audio("yes.wav");
 var no = new Audio("no.wav");
-
-
-
-function wypisz_haslo()
-{
-
-	document.getElementById("plansza").innerHTML = haslo1;
-}
+var letters = 'aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż'.toUpperCase();
+var messages = {
+	'win': 'Tak jest! Podano prawidłowe hasło: ',
+	'lose': 'Przegrana :( Prawidłowe hasło to: ',
+	'try_again': '<br/><br/><span class="reset" onclick="restart()">JESZCZE RAZ?</span>',
+};
 
 window.onload = start;
-
-var litery = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż"
-litery = litery.toUpperCase();
-
 function start()
 {
-	var tresc_diva = "";
-
-
-	for(i=0;i<35;i++){
-		var litera = litery.charAt(i)
-		tresc_diva = tresc_diva + '<div onclick="sprawdz('+i+')" class="litera" id="lit'+i+'">'+litera+'</div>';
-		if ((i+1) % 7 == 0) tresc_diva = tresc_diva + '<div style="clear:both;"></div>';
-
-		window.console.log(pojemnik);
-
-	}
-
-	document.getElementById("alfabet").innerHTML =  tresc_diva;
-
-	wypisz_haslo();
-}
-
-String.prototype.ustawZnak = function(miejsce, znak){
-	if (miejsce > this.length-1)return this.toString();
-	else return this.substr(0, miejsce) + znak + this.substr(miejsce + 1);
-}
-
-
-function sprawdz(nr){
-
-	var trafiona = false;
-
-	for (i=0; i<dlugosc; i++){
-		if (haslo.charAt(i) == litery.charAt(nr) ){
-			haslo1 = haslo1.ustawZnak(i, litery.charAt(nr))
-			trafiona = true;
+	setStatus(numWins);
+	var html = '';
+	for(i = 0; i < letters.length; ++i){
+		var letter = letters.charAt(i);
+		html += '<div onclick="check('+i+')" class="letter" id="lit'+i+'">'
+					+ letter +
+				'</div>';
+		if(isDivide(i + 1, 7))
+		{
+			html += '<div style="clear:both;"></div>';
 		}
 	}
-	if (trafiona == true){
-		yes.play();
-		var element = "lit" + nr;
-		document.getElementById(element).style.background = "#003300";
-		document.getElementById(element).style.color = "#00c000";
-		document.getElementById(element).style.border = "3px solid #00c000";
-		document.getElementById(element).style.cursor = "default";
-		wypisz_haslo();
-	}
-	else{
-		no.play();
-		var element = "lit" + nr;
-		document.getElementById(element).style.background = "#330000";
-		document.getElementById(element).style.color = "#C00000";
-		document.getElementById(element).style.border = "3px solid #C00000";
-		document.getElementById(element).style.cursor = "default";
-		document.getElementById(element).setAttribute("onclick",";");
+	document.getElementById('alphabetID').innerHTML =  html;
+	setPassword();
+}
 
-		//skucha
-		ile_skuch++;
-		var obraz = "img/s" + ile_skuch + ".jpg";
-		document.getElementById("szubienica").innerHTML = '<img src="'+obraz+'"alt=""/>';
-	}
 
-	//wygrana
-	if (haslo == haslo1){
-		document.getElementById("alfabet").innerHTML = "Tak jest! Podano prawidłowe hasło: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+String.prototype.setChar = function(place, sign){
+	if (place > this.length - 1)
+	{
+		return this.toString();
+	}
+	else
+	{
+		return this.substr(0, place) + sign + this.substr(place + 1);
+	}
+};
+
+
+function check(number){
+	var hit = false;
+	for (i = 0; i < haslo.length; ++i){
+		if (haslo.charAt(i) === letters.charAt(number))
+		{
+			hiddenPassword = hiddenPassword.setChar(i, letters.charAt(number));
+			hit = true;
+		}
 	}
 
-	//przegrana
-	if (ile_skuch >= 9){
-		document.getElementById("alfabet").innerHTML = "Przegrana :( Prawidłowe hasło to: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+	if (hit === true)
+	{
+		checkSuccess(number);
+	}
+	else
+	{
+		checkFail(number);
 	}
 
+	let alfabetElem = document.getElementById('alphabetID');
+	if (haslo === hiddenPassword)
+	{
+		alfabetElem.innerHTML = messages['win'] + haslo + messages['try_again'];
+		setStatus(++numWins);
+	}
+	if (numFails >= FAIL_LIMIT)
+	{
+		alfabetElem.innerHTML = messages['lose'] + haslo + messages['try_again'];
+	}
+	setPassword();
+}
 
-	wypisz_haslo();
+
+function setPassword()
+{
+	document.getElementById('boardID').innerHTML = hiddenPassword;
+}
+
+
+function isDivide(number, divider)
+{
+	return number % divider === 0;
+}
+
+
+function setStatus(wins)
+{
+	let allProverbs = PROVERBS.length;
+	document.getElementById('statusID').innerHTML = wins + '/' + allProverbs;
+}
+
+
+function restart()
+{
+	resetVariables();
+	start();
+}
+
+
+function resetVariables()
+{
+	haslo = getProverb();
+	numFails = 0;
+	hiddenPassword = hidePassword(haslo);
+}
+
+
+function checkFail(number)
+{
+	var letterElem = document.getElementById('lit' + number);
+	no.play();
+	letterElem.style.background = '#330000';
+	letterElem.style.color = '#C00000';
+	letterElem.style.border = '3px solid #C00000';
+	letterElem.style.cursor = 'default';
+	letterElem.setAttribute('onclick',';');
+	numFails++;
+	var picture = 'img/s' + numFails + '.jpg';
+	document.getElementById('gallowsID').innerHTML = '<img src="' + picture + '" alt=""/>';
+}
+
+
+function checkSuccess(number)
+{
+	var letterElem = document.getElementById('lit' + number);
+	yes.play();
+	letterElem.style.background = '#003300';
+	letterElem.style.color = '#00c000';
+	letterElem.style.border = '3px solid #00c000';
+	letterElem.style.cursor = 'default';
+	setPassword();
+}
+
+
+function resetProverbs()
+{
+	proverbs = [...PROVERBS];
 }
